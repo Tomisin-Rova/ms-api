eb62b6b35d979684d73d78078c216e7c17e812af
Lekan Adigun <adigunhammed.lekan@gmail.com>
2020-09-09 10:19:03 +0100

    refactored to fit functions in mutation.resolvers.go

--- graph/mutation.resolvers.go
+++ graph/mutation.resolvers.go
@@ -5,8 +5,11 @@ package graph
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"ms.api/graph/generated"
+	onboarding "ms.api/protos/pb/onboardingService"
+	verify "ms.api/protos/pb/verifyService"
 	"ms.api/types"
 )
 
@@ -16,6 +19,30 @@ func (r *mutationResolver) SubmitLiveVideo(ctx context.Context, id string) (*typ
 
 func (r *mutationResolver) PingKYCService(ctx context.Context, message string) (*types.Result, error) {
 	panic(fmt.Errorf("not implemented"))
+}
+
+func (r *mutationResolver) CreatePhone(ctx context.Context, input types.CreatePhoneInput) (*types.Result, error) {
+	if input.Phone == "" || len(input.Phone) < 6 {
+		return nil, errors.New("invalid phone number")
+	}
+	result, err := r.onBoardingService.CreatePhone(ctx,
+		&onboarding.CreatePhoneRequest{PhoneNumber: input.Phone, Device: &onboarding.Device{Os: input.Device.Os}})
+	if err != nil {
+		r.logger.Infof("onBoardingService.createPhone() failed: %v", err)
+		return nil, err
+	}
+	return &types.Result{Message: result.Message, Success: true}, nil
+}
+
+func (r *mutationResolver) VerifyOtp(ctx context.Context, phone string, code string) (*types.Result, error) {
+	resp, err := r.verifyService.VerifySmsOtp(context.Background(), &verify.OtpVerificationRequest{
+		Phone: phone, Code: code,
+	})
+	if err != nil {
+		r.logger.Infof("verifyService.verifySmsOtp() failed: %v", err)
+		return nil, err
+	}
+	return &types.Result{Success: resp.Match, Message: resp.Message}, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
