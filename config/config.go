a153da0f5fb2b57b925dc1eaaeeba8235c41d463
Xanik <sandraike57@gmail.com>
2020-12-16 02:20:25 +0100

    BANK-890/FEAT: adds query to fetch payees
    - adds query to get payees by phone numbers

--- config/config.go
+++ config/config.go
@@ -23,6 +23,7 @@ type Secrets struct {
 	ProductServiceURL    string
 	PayeeServiceURL      string
 	PersonServiceURL     string
+	PayeeServiceURL      string
 	VaultAddress         string        `json:"vault_address"`
 	VaultToken           string        `json:"vault_token"`
 	VaultSecretsPath     string        `json:"vault_secrets_path"`
@@ -64,6 +65,7 @@ func LoadSecrets() (*Secrets, error) {
 	ss.ProductServiceURL = os.Getenv("PRODUCT_SERVICE_URL")
 	ss.PayeeServiceURL = os.Getenv("PAYEE_SERVICE_URL")
 	ss.PersonServiceURL = os.Getenv("PERSON_SERVICE_URL")
+	ss.PayeeServiceURL = os.Getenv("PAYEE_SERVICE_URL")
 	if err := ss.Environment.IsValid(); err != nil {
 		log.Error("Error in environment variables: %v", err)
 	}
