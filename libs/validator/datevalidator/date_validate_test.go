ee656e1af0c295eec98e2d9952276ef710a904a9
Eduardo Yanez <eduardo@roava.app>
2022-01-18 14:23:07 -0600

    RVA-000|fix: FIx dob template

--- libs/validator/datevalidator/date_validate_test.go
+++ libs/validator/datevalidator/date_validate_test.go
@@ -13,17 +13,17 @@ func TestValidateDob(t *testing.T) {
 		err error
 	}
 	cases := []testCase{
-		{dob: "12/10/1990", err: nil},
-		{dob: "12/10/190", err: ErrInvalidFormat},
-		{dob: "1a/10/1990", err: ErrInvalidFormat},
-		{dob: "11/10/990", err: ErrInvalidFormat},
-		{dob: "11/1a/1990", err: ErrInvalidFormat},
-		{dob: "40/10/1990", err: ErrInvalidFormat},
-		{dob: "40/10/1990", err: ErrInvalidFormat},
-		{dob: "01/15/1990", err: ErrInvalidFormat},
-		{dob: "01/11/19b0", err: ErrInvalidFormat},
-		{dob: "01/01/2020", err: ErrInvalidAge},
-		{dob: "01/01/2003", err: nil},
+		{dob: "12-10-1990", err: nil},
+		{dob: "12-10-190", err: ErrInvalidFormat},
+		{dob: "1a-10-1990", err: ErrInvalidFormat},
+		{dob: "11-10-990", err: ErrInvalidFormat},
+		{dob: "11-1a-1990", err: ErrInvalidFormat},
+		{dob: "40-10-1990", err: ErrInvalidFormat},
+		{dob: "40-10-1990", err: ErrInvalidFormat},
+		{dob: "01-15-1990", err: ErrInvalidFormat},
+		{dob: "01-11-19b0", err: ErrInvalidFormat},
+		{dob: "01-01-2020", err: ErrInvalidAge},
+		{dob: "01-01-2003", err: nil},
 	}
 	for idx, c := range cases {
 		t.Run(fmt.Sprintf("case-%d", idx), func(t *testing.T) {
--- mocks/auth_mock.go
+++ mocks/auth_mock.go
@@ -6,77 +6,36 @@ package mocks
 
 import (
 	context "context"
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
 	grpc "google.golang.org/grpc"
 	auth "ms.api/protos/pb/auth"
+	reflect "reflect"
 )
 
-// MockAuthServiceClient is a mock of AuthServiceClient interface.
+// MockAuthServiceClient is a mock of AuthServiceClient interface
 type MockAuthServiceClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockAuthServiceClientMockRecorder
 }
 
-// MockAuthServiceClientMockRecorder is the mock recorder for MockAuthServiceClient.
+// MockAuthServiceClientMockRecorder is the mock recorder for MockAuthServiceClient
 type MockAuthServiceClientMockRecorder struct {
 	mock *MockAuthServiceClient
 }
 
-// NewMockAuthServiceClient creates a new mock instance.
+// NewMockAuthServiceClient creates a new mock instance
 func NewMockAuthServiceClient(ctrl *gomock.Controller) *MockAuthServiceClient {
 	mock := &MockAuthServiceClient{ctrl: ctrl}
 	mock.recorder = &MockAuthServiceClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use.
+// EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockAuthServiceClient) EXPECT() *MockAuthServiceClientMockRecorder {
 	return m.recorder
 }
 
-// Login mocks base method.
-func (m *MockAuthServiceClient) Login(ctx context.Context, in *auth.LoginRequest, opts ...grpc.CallOption) (*auth.TokenPairResponse, error) {
-	m.ctrl.T.Helper()
-	varargs := []interface{}{ctx, in}
-	for _, a := range opts {
-		varargs = append(varargs, a)
-	}
-	ret := m.ctrl.Call(m, "Login", varargs...)
-	ret0, _ := ret[0].(*auth.TokenPairResponse)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// Login indicates an expected call of Login.
-func (mr *MockAuthServiceClientMockRecorder) Login(ctx, in interface{}, opts ...interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	varargs := append([]interface{}{ctx, in}, opts...)
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockAuthServiceClient)(nil).Login), varargs...)
-}
-
-// RefreshToken mocks base method.
-func (m *MockAuthServiceClient) RefreshToken(ctx context.Context, in *auth.RefreshTokenRequest, opts ...grpc.CallOption) (*auth.TokenPairResponse, error) {
-	m.ctrl.T.Helper()
-	varargs := []interface{}{ctx, in}
-	for _, a := range opts {
-		varargs = append(varargs, a)
-	}
-	ret := m.ctrl.Call(m, "RefreshToken", varargs...)
-	ret0, _ := ret[0].(*auth.TokenPairResponse)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// RefreshToken indicates an expected call of RefreshToken.
-func (mr *MockAuthServiceClientMockRecorder) RefreshToken(ctx, in interface{}, opts ...interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	varargs := append([]interface{}{ctx, in}, opts...)
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RefreshToken", reflect.TypeOf((*MockAuthServiceClient)(nil).RefreshToken), varargs...)
-}
-
-// Signup mocks base method.
+// Signup mocks base method
 func (m *MockAuthServiceClient) Signup(ctx context.Context, in *auth.SignupRequest, opts ...grpc.CallOption) (*auth.TokenPairResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
@@ -89,14 +48,34 @@ func (m *MockAuthServiceClient) Signup(ctx context.Context, in *auth.SignupReque
 	return ret0, ret1
 }
 
-// Signup indicates an expected call of Signup.
+// Signup indicates an expected call of Signup
 func (mr *MockAuthServiceClientMockRecorder) Signup(ctx, in interface{}, opts ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Signup", reflect.TypeOf((*MockAuthServiceClient)(nil).Signup), varargs...)
 }
 
-// StaffLogin mocks base method.
+// Login mocks base method
+func (m *MockAuthServiceClient) Login(ctx context.Context, in *auth.LoginRequest, opts ...grpc.CallOption) (*auth.TokenPairResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "Login", varargs...)
+	ret0, _ := ret[0].(*auth.TokenPairResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Login indicates an expected call of Login
+func (mr *MockAuthServiceClientMockRecorder) Login(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockAuthServiceClient)(nil).Login), varargs...)
+}
+
+// StaffLogin mocks base method
 func (m *MockAuthServiceClient) StaffLogin(ctx context.Context, in *auth.StaffLoginRequest, opts ...grpc.CallOption) (*auth.TokenPairResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
@@ -109,14 +88,34 @@ func (m *MockAuthServiceClient) StaffLogin(ctx context.Context, in *auth.StaffLo
 	return ret0, ret1
 }
 
-// StaffLogin indicates an expected call of StaffLogin.
+// StaffLogin indicates an expected call of StaffLogin
 func (mr *MockAuthServiceClientMockRecorder) StaffLogin(ctx, in interface{}, opts ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "StaffLogin", reflect.TypeOf((*MockAuthServiceClient)(nil).StaffLogin), varargs...)
 }
 
-// ValidateToken mocks base method.
+// RefreshToken mocks base method
+func (m *MockAuthServiceClient) RefreshToken(ctx context.Context, in *auth.RefreshTokenRequest, opts ...grpc.CallOption) (*auth.TokenPairResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{ctx, in}
+	for _, a := range opts {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "RefreshToken", varargs...)
+	ret0, _ := ret[0].(*auth.TokenPairResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// RefreshToken indicates an expected call of RefreshToken
+func (mr *MockAuthServiceClientMockRecorder) RefreshToken(ctx, in interface{}, opts ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{ctx, in}, opts...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RefreshToken", reflect.TypeOf((*MockAuthServiceClient)(nil).RefreshToken), varargs...)
+}
+
+// ValidateToken mocks base method
 func (m *MockAuthServiceClient) ValidateToken(ctx context.Context, in *auth.ValidateTokenRequest, opts ...grpc.CallOption) (*auth.ValidateTokenResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{ctx, in}
@@ -129,67 +128,37 @@ func (m *MockAuthServiceClient) ValidateToken(ctx context.Context, in *auth.Vali
 	return ret0, ret1
 }
 
-// ValidateToken indicates an expected call of ValidateToken.
+// ValidateToken indicates an expected call of ValidateToken
 func (mr *MockAuthServiceClientMockRecorder) ValidateToken(ctx, in interface{}, opts ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{ctx, in}, opts...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ValidateToken", reflect.TypeOf((*MockAuthServiceClient)(nil).ValidateToken), varargs...)
 }
 
-// MockAuthServiceServer is a mock of AuthServiceServer interface.
+// MockAuthServiceServer is a mock of AuthServiceServer interface
 type MockAuthServiceServer struct {
 	ctrl     *gomock.Controller
 	recorder *MockAuthServiceServerMockRecorder
 }
 
-// MockAuthServiceServerMockRecorder is the mock recorder for MockAuthServiceServer.
+// MockAuthServiceServerMockRecorder is the mock recorder for MockAuthServiceServer
 type MockAuthServiceServerMockRecorder struct {
 	mock *MockAuthServiceServer
 }
 
-// NewMockAuthServiceServer creates a new mock instance.
+// NewMockAuthServiceServer creates a new mock instance
 func NewMockAuthServiceServer(ctrl *gomock.Controller) *MockAuthServiceServer {
 	mock := &MockAuthServiceServer{ctrl: ctrl}
 	mock.recorder = &MockAuthServiceServerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use.
+// EXPECT returns an object that allows the caller to indicate expected use
 func (m *MockAuthServiceServer) EXPECT() *MockAuthServiceServerMockRecorder {
 	return m.recorder
 }
 
-// Login mocks base method.
-func (m *MockAuthServiceServer) Login(arg0 context.Context, arg1 *auth.LoginRequest) (*auth.TokenPairResponse, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Login", arg0, arg1)
-	ret0, _ := ret[0].(*auth.TokenPairResponse)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// Login indicates an expected call of Login.
-func (mr *MockAuthServiceServerMockRecorder) Login(arg0, arg1 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockAuthServiceServer)(nil).Login), arg0, arg1)
-}
-
-// RefreshToken mocks base method.
-func (m *MockAuthServiceServer) RefreshToken(arg0 context.Context, arg1 *auth.RefreshTokenRequest) (*auth.TokenPairResponse, error) {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "RefreshToken", arg0, arg1)
-	ret0, _ := ret[0].(*auth.TokenPairResponse)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-// RefreshToken indicates an expected call of RefreshToken.
-func (mr *MockAuthServiceServerMockRecorder) RefreshToken(arg0, arg1 interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RefreshToken", reflect.TypeOf((*MockAuthServiceServer)(nil).RefreshToken), arg0, arg1)
-}
-
-// Signup mocks base method.
+// Signup mocks base method
 func (m *MockAuthServiceServer) Signup(arg0 context.Context, arg1 *auth.SignupRequest) (*auth.TokenPairResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Signup", arg0, arg1)
@@ -198,13 +167,28 @@ func (m *MockAuthServiceServer) Signup(arg0 context.Context, arg1 *auth.SignupRe
 	return ret0, ret1
 }
 
-// Signup indicates an expected call of Signup.
+// Signup indicates an expected call of Signup
 func (mr *MockAuthServiceServerMockRecorder) Signup(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Signup", reflect.TypeOf((*MockAuthServiceServer)(nil).Signup), arg0, arg1)
 }
 
-// StaffLogin mocks base method.
+// Login mocks base method
+func (m *MockAuthServiceServer) Login(arg0 context.Context, arg1 *auth.LoginRequest) (*auth.TokenPairResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Login", arg0, arg1)
+	ret0, _ := ret[0].(*auth.TokenPairResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// Login indicates an expected call of Login
+func (mr *MockAuthServiceServerMockRecorder) Login(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Login", reflect.TypeOf((*MockAuthServiceServer)(nil).Login), arg0, arg1)
+}
+
+// StaffLogin mocks base method
 func (m *MockAuthServiceServer) StaffLogin(arg0 context.Context, arg1 *auth.StaffLoginRequest) (*auth.TokenPairResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "StaffLogin", arg0, arg1)
@@ -213,13 +197,28 @@ func (m *MockAuthServiceServer) StaffLogin(arg0 context.Context, arg1 *auth.Staf
 	return ret0, ret1
 }
 
-// StaffLogin indicates an expected call of StaffLogin.
+// StaffLogin indicates an expected call of StaffLogin
 func (mr *MockAuthServiceServerMockRecorder) StaffLogin(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "StaffLogin", reflect.TypeOf((*MockAuthServiceServer)(nil).StaffLogin), arg0, arg1)
 }
 
-// ValidateToken mocks base method.
+// RefreshToken mocks base method
+func (m *MockAuthServiceServer) RefreshToken(arg0 context.Context, arg1 *auth.RefreshTokenRequest) (*auth.TokenPairResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "RefreshToken", arg0, arg1)
+	ret0, _ := ret[0].(*auth.TokenPairResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// RefreshToken indicates an expected call of RefreshToken
+func (mr *MockAuthServiceServerMockRecorder) RefreshToken(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "RefreshToken", reflect.TypeOf((*MockAuthServiceServer)(nil).RefreshToken), arg0, arg1)
+}
+
+// ValidateToken mocks base method
 func (m *MockAuthServiceServer) ValidateToken(arg0 context.Context, arg1 *auth.ValidateTokenRequest) (*auth.ValidateTokenResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "ValidateToken", arg0, arg1)
@@ -228,7 +227,7 @@ func (m *MockAuthServiceServer) ValidateToken(arg0 context.Context, arg1 *auth.V
 	return ret0, ret1
 }
 
-// ValidateToken indicates an expected call of ValidateToken.
+// ValidateToken indicates an expected call of ValidateToken
 func (mr *MockAuthServiceServerMockRecorder) ValidateToken(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ValidateToken", reflect.TypeOf((*MockAuthServiceServer)(nil).ValidateToken), arg0, arg1)
